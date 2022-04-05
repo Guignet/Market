4ac703b68d6315c9d73f803683dda026b0308567
Guignet <guillaume.peugnet.2001@gmail.com>
2022-04-05 15:20:26 +0200

    final fixes

--- demo/src/main/java/com/project/marketsimulator/web/MagasinController.java
+++ demo/src/main/java/com/project/marketsimulator/web/MagasinController.java
@@ -1,10 +1,12 @@
 package com.project.marketsimulator.web;
 
+import com.project.marketsimulator.dao.IMemoryJPA;
 import com.project.marketsimulator.model.Magasin;
 import com.project.marketsimulator.services.MagasinService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
@@ -21,7 +23,7 @@ public class MagasinController {
     @GetMapping
     public String getAll(Model model){
         List<Magasin> magasins = service.getAll();
-        model.addAllAttribute("magasins",magasins);
+        model.addAttribute("magasins",magasins);
         return "magasins";
     }
 }
