8847deffa4c22e7998771027b946c6b2d792d32e
Charlotte Zanardi <cha.zanardi@gmail.com>
2022-04-05 14:13:29 +0200

    remaning in english

---
+++ demo/src/main/java/com/project/marketsimulator/dao/ProduitDAO.java
@@ -0,0 +1,33 @@
+package com.project.marketsimulator.dao;
+
+import com.project.marketsimulator.model.produit.Produit;
+
+import java.util.List;
+
+public class ProduitDAO implements IGenericDAO<Produit, Long> {
+
+    @Override
+    public void add(Produit data) {
+        
+    }
+
+    @Override
+    public void update(Produit data) {
+
+    }
+
+    @Override
+    public void delete(Long aLong) {
+
+    }
+
+    @Override
+    public List<Produit> getAll() {
+        return null;
+    }
+
+    @Override
+    public Produit getById(Long aLong) {
+        return null;
+    }
+}
